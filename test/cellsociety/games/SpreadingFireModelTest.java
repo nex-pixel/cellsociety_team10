a7434666af21eca510df865c330b7628c8868352
Haseeb Chaudhry <hac21@duke.edu>
2021-10-25 17:28:03 -0400

    made class for wat-or world

---
+++ test/cellsociety/games/SpreadingFireModelTest.java
@@ -0,0 +1,21 @@
+package cellsociety.games;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpreadingFireModelTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void applyRule() {
+    }
+}
