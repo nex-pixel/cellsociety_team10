56c9d09b0d5e25dd14116de678b52b7df127227a
Young Jun <yj112@duke.edu>
2021-10-30 17:41:39 -0400

    added clicked on functionality for triangle cell

--- src/cellsociety/view/TriangleCell.java
+++ src/cellsociety/view/TriangleCell.java
@@ -1,16 +1,15 @@
 package cellsociety.view;
 
+import cellsociety.controller.SimulatorController;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.TriangleMesh;
 
 public class TriangleCell extends Polygon {
-    private int xCoordinate;
-    private int yCoordinate;
 
-    public TriangleCell(int orientation, int xCoordinate, int yCoordinate){
+
+    public TriangleCell(SimulatorController simulatorController, int orientation, int xCoordinate, int yCoordinate){
         if(orientation == 0) this.getPoints().addAll(0.0,0.0, 50.0,0.0, 25.0,43.30125);
         if(orientation == 1) this.getPoints().addAll(0.0, 43.30125, 25.0, 0.0, 50.0, 43.30125);
-        this.xCoordinate = xCoordinate;
-        this.yCoordinate = yCoordinate;
+        this.setOnMouseClicked(event -> simulatorController.updateCellOnClick(xCoordinate, yCoordinate));
     }
 }
