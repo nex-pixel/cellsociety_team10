72baac741cd423fc538a77d752e0be8d44e1d19c
Young Jun <yj112@duke.edu>
2021-10-30 17:30:04 -0400

    added clicked on functionality for square cell

--- src/cellsociety/view/SquareCell.java
+++ src/cellsociety/view/SquareCell.java
@@ -1,14 +1,18 @@
 package cellsociety.view;
 
 import cellsociety.components.Cell;
+import cellsociety.controller.SimulatorController;
 import javafx.scene.Node;
 import javafx.scene.shape.Rectangle;
 
 import java.util.Map;
 
 public class SquareCell extends Rectangle {
-    // TODO: add method to handle setOnMouseClicked
-    public SquareCell(){
+
+    public SquareCell(SimulatorController simulatorController, int xCoordinate, int yCoordinate){
+
+        this.setOnMouseClicked(event -> simulatorController.updateCellOnClick(xCoordinate, yCoordinate));
+
     }
 
 }
