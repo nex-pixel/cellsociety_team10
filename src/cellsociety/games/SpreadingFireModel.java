c4b268031df2a9410a14eee24771cbffaa760086
Haseeb Chaudhry <hac21@duke.edu>
2021-11-01 01:14:41 -0400

    did the randomized grid creation for

--- src/cellsociety/games/SpreadingFireModel.java
+++ src/cellsociety/games/SpreadingFireModel.java
@@ -13,6 +13,8 @@ public class SpreadingFireModel extends Game{
     private double probGrow;
     private boolean firePresentInGrid = true;
     private int expandGridByInt;
+    private int NUM_STATES = 3;
+    private final int DEFAULT_GRID_CHOICE = 0;
 
     public SpreadingFireModel(String filename) {
         super(filename);
@@ -24,6 +26,18 @@ public class SpreadingFireModel extends Game{
         if(TEST == false) {
             getGrid().expandGrid(expandGridByInt, expandGridByInt, expandGridByInt, expandGridByInt);
         }
+    }
+
+    public SpreadingFireModel (int numCols, int numRows){
+        super(numCols, numRows);
+        setNumStatesOnBoard(NUM_STATES);
+        setGrid(createRandomIntTwoDArray(numCols, numRows), DEFAULT_GRID_CHOICE, DEFAULT_GRID_CHOICE, DEFAULT_GRID_CHOICE);
+    }
+
+    public SpreadingFireModel (int numCols, int numRows, int gridType, int neighborMode, int edgePolicy) {
+        super(numCols, numRows, gridType, neighborMode, edgePolicy);
+        setNumStatesOnBoard(NUM_STATES);
+        setGrid(createRandomIntTwoDArray(numCols, numRows), gridType, neighborMode, edgePolicy);
     }
 
     public SpreadingFireModel (String filename, int gridType, int neighborMode, int edgePolicy) {
