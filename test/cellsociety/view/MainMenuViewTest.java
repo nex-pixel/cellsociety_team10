aa6778b2411efb7682d2188d202d76fba22aef48
Young Jun <yj112@duke.edu>
2021-11-02 01:04:45 -0400

    added MainMenuView tests

--- test/cellsociety/view/MainMenuViewTest.java
+++ test/cellsociety/view/MainMenuViewTest.java
@@ -1,73 +1,68 @@
 package cellsociety.view;
 
 import cellsociety.controller.MainController;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Labeled;
-import javafx.scene.input.KeyCode;
-
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import util.DukeApplicationTest;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ResourceBundle;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class MainMenuViewTest extends DukeApplicationTest {
-    MainMenuView testView;
-    private Labeled testLabel;
-    private Labeled testLabel2;
-    private Labeled testLabel3;
-    private ResourceBundle langResourceBundle = ResourceBundle.getBundle("cellsociety.resources.English");
-    private ResourceBundle eventResourceBundle = ResourceBundle.getBundle("cellsociety.resources.ActionEvents");
+    private Scene testScene;
+
+    private ResourceBundle langResourceBundle;
+    private MainController mainController;
+    private MainMenuView testView;
+    private Stage myStage;
+
 
     @Override
     public void start(Stage stage) throws IOException {
+        langResourceBundle = ResourceBundle.getBundle("cellsociety.resources.languages.English");
         testView = new MainMenuView(langResourceBundle);
-        stage.show();
-        testLabel2 = lookup("Select a type of simulation to run").query();
-        testLabel3 = lookup("Load File").query();
+        myStage = stage;
+        mainController = new MainController(stage, langResourceBundle);
+        myStage.setScene(testView.setMenuDisplay(new MainController(myStage, langResourceBundle),500, 500 ));
+        myStage.show();
+    }
+
+
+    @Test
+    void testSetMenuDisplayWidth(){
+        Scene testScene = testView.setMenuDisplay(mainController, 500, 600);
+        Assertions.assertEquals(500, testScene.getWidth(), "setMenuDisplay creates a scene with a wrong width");
     }
 
     @Test
-    void testLanguageButton(){
-        Button languageButton = lookup("Select a language").query();
-        clickOn(languageButton);
-        Button okButton = lookup("OK").query();
-        clickOn(okButton);
-        //TODO: Our project currently only supports English. Add assertion and test the method once multiple languages are supported
+    void testSetMenuDisplayHeight(){
+        Scene testScene = testView.setMenuDisplay(mainController, 500, 600);
+        Assertions.assertEquals(600, testScene.getHeight(), "setMenuDisplay creates a scene with a wrong width");
     }
 
     @Test
-    void testSimulationButton(){
-        Button simulationButton = lookup("Select a type of simulation to run").query();
-        clickOn(simulationButton);
-        Button okButton = lookup("OK").query();
-        clickOn(okButton);
-        //TODO: Our project currently only supports one simulation. Add assertion and test the method once multiple simulations are supported
+    void testSetMenuDisplay(){
+        Button gameTypeButton = lookup("Select a type of simulation to run").query();
+        clickOn(gameTypeButton);
+        clickOn("OK");
+        Button colorButton = lookup("Choose color scheme").query();
+        clickOn(colorButton);
+        clickOn("OK");
+        Button cellTypeButton = lookup("Choose Cell Type").query();
+        clickOn(cellTypeButton);
+        clickOn("OK");
+        Button neighborButton = lookup("Choose Neighbor Mode").query();
+        clickOn(neighborButton);
+        clickOn("OK");
+        Button edgeButton = lookup("Choose Edge Policy").query();
+        clickOn(edgeButton);
+        clickOn("OK");
+        Button newButton = lookup("Create New Simulation").query();
+        clickOn(newButton);
     }
-
-    @Test
-    void testLoadButton(){
-        Button loadButton = lookup("Load File").query();
-        clickOn(loadButton);
-        press(KeyCode.RIGHT);
-        for(int i = 0; i < 5; i++){
-            press(KeyCode.DOWN);
-        }
-        press(KeyCode.RIGHT);
-        press(KeyCode.DOWN);
-        press(KeyCode.RIGHT);
-        press(KeyCode.ENTER);
-        //TODO: find a way to choose a file
-    }
-
-    @Test
-    void testRunSimulationButton(){
-        Button simulationButton = lookup("Create New Simulation").query();
-        clickOn(simulationButton);
-    }
-
 }
+
