d5eb9ebbfd6cf3639bbd6e01a042efcef01900be
Haseeb Chaudhry <hac21@duke.edu>
2021-11-02 02:41:32 -0400

    finished javadocs for file reader

--- src/cellsociety/components/filereader/ReadFile.java
+++ src/cellsociety/components/filereader/ReadFile.java
@@ -1,5 +1,16 @@
 package cellsociety.components.filereader;
 
+/***
+ * This class creates an abstract ReadFile object that can be used in its subclasses to create specific file readers such as
+ * a sub class to read a JSON or one to read CSV.
+ *
+ * The class is call by sub grid classes to create 2D array from different file types
+ * Dependencies: NONE
+ * Assumptions: The files will be written in a specific format and don't need variable readers.
+ * Example: Create a ReadFile Object that reads in a specific file format and converts it into a 2D array to be used in grid creation.
+ *
+ * @author Norah Tan
+ */
 
 public abstract class ReadFile {
 
