e3024dfaf6a2ba90ecbaf982cc3e0d38af2e0a2a
Young Jun <yj112@duke.edu>
2021-11-01 20:38:12 -0400

    debugged error when cells are clicked

--- test/cellsociety/view/MainMenuViewTest.java
+++ test/cellsociety/view/MainMenuViewTest.java
@@ -1,5 +1,6 @@
 package cellsociety.view;
 
+import cellsociety.controller.MainController;
 import javafx.scene.control.Button;
 import javafx.scene.control.Labeled;
 import javafx.scene.input.KeyCode;
@@ -24,7 +25,7 @@ class MainMenuViewTest extends DukeApplicationTest {
 
     @Override
     public void start(Stage stage) throws IOException {
-        //testView = new MainMenuView(langResourceBundle, eventResourceBundle);
+        testView = new MainMenuView(langResourceBundle);
         stage.show();
         testLabel2 = lookup("Select a type of simulation to run").query();
         testLabel3 = lookup("Load File").query();
